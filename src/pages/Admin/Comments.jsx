b8c19c56f1950d81cd64f5ae5e89f1e5faf0fc0a
Tapas kundu <tapaskundu3762@gmail.com>
2025-08-01 23:12:05 +0530

    started comment page

--- src/pages/Admin/Comments.jsx
+++ src/pages/Admin/Comments.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { comments_data } from '../../assets/assets.js'
+const Comments = ({comment,fetchComments}) => {
 
-const Comments = () => {
+  const [comments, setComments]= useState([]);
+  const [filter, setFilter]= useState('Not Approved');
+
+  const {blog, createdAt, _id}= comment;
+  
+
+    
+  const fetchComments= async()=>{
+
+    setComments(comments_data);
+
+  }
+  useEffect(()=>{
+    fetchComments();
+    
+  },[])
+
   return (
-    <div>Comments</div>
+    <div>
+      
+    </div>
   )
 }
 
